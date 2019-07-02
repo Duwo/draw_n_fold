c695ac61bbbd972fce3f73b69dd3918bdf26b143
Mikael <mikael@storytel.com>
2019-07-02 14:28:24 +0200

    Add is_done method stub in Picture model.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,9 @@
 class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
+
+  def is_done
+  	return false #TODO: add something that iterates through parts and returns false if it finds one with image_part=null. 
+  end
+
 end
+
