9119d17146775491a8f56b835548b06a44d5f88d
Mikael <mikael@storytel.com>
2019-07-02 17:48:54 +0200

    yeah.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -7,5 +7,22 @@ class Picture < ApplicationRecord
     PicturePart.generate_parts(picture, picture_size)
     return picture
   end
+
+  def done?
+  	picture_parts.not_done.empty?
+  end
+
+
+  def combine 
+  	image_list = Magick::ImageList.new
+  	picture_parts.each do |part|
+  		image_list << Magick::Image.read(part.image_part.url) 
+  	end
+
+  	image_list.write("combine.png")
+  	image = "combine.png"
+
+  end
+
 end
 
